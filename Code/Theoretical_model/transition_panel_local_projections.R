9de45319d407f8588f9f6a4ef6e23e89b6fd8bb1
Rodney-Menezes <rodney.menezes@pucp.edu.pe>
2025-09-20 18:57:53 -0500

    Update transition_panel_local_projections.R

--- Code/Theoretical_model/transition_panel_local_projections.R
+++ Code/Theoretical_model/transition_panel_local_projections.R
@@ -120,7 +120,7 @@ run_local_projection <- function(df, interaction_col, horizon) {
   model <- fixest::feols(
     fml = response ~ interaction,
     data = reg_data,
-    fixef = ~firm_id + quarter_id,
+    fixef = c("firm_id", "quarter_id"),
     cluster = ~firm_id
   )
 
